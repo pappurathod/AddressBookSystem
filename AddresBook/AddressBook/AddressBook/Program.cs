508ccb36e64e760a98507c7754d0756144a5993c
pappurathod <prathod4545@gmail.com>
2022-03-19 13:51:52 +0530

    Edit Existing Contact Using Person Name

--- AddresBook/AddressBook/AddressBook/Program.cs
+++ AddresBook/AddressBook/AddressBook/Program.cs
@@ -1,74 +1,137 @@
-﻿//UC2
+﻿using System;
+using System.Collections.Generic;
 
-namespace AddressBook
+namespace AddresBook
 {
-    class AddresBook
+    class Person
     {
-        public string FirstName;
-        public string LastName;
-        public string Address;
-        public string City;
-        public string State;
-        public string zipcode;
-        public string PhoneNumber;
-        public string EmailId;
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public string zipCode;
+        public string phoneNumber;
+        public string emailId;
     }
+
     public class PersonDetail
     {
-        AddresBook addbook = new AddresBook();
-        public void SetPersonDetail()
+        List<Person> listgetDetail;
+        public PersonDetail()
         {
-            Console.WriteLine("Enter First name");
-            addbook.FirstName = Console.ReadLine();
-            Console.WriteLine("Enter your Last name");
-            addbook.LastName = Console.ReadLine();
-            Console.WriteLine("Enter your Address");
-            addbook.Address = Console.ReadLine();
-            Console.WriteLine("Enter City");
-            addbook.City = Console.ReadLine();
-            Console.WriteLine("enter State");
-            addbook.State = Console.ReadLine();
-            Console.WriteLine("Enter your Zipcode");
-            addbook.zipcode = Console.ReadLine();
-            Console.WriteLine("Enter your Phone number");
-            addbook.PhoneNumber = Console.ReadLine();
-            Console.WriteLine("enter your email id");
-            addbook.EmailId = Console.ReadLine();
+            listgetDetail = new List<Person>();
+        }
+        public void PersonAddress()
+        {
+            Person person = new Person();
+            Console.WriteLine("Enter first name");
+            person.firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last name");
+            person.lastName = Console.ReadLine();
+            Console.WriteLine("Enter address");
+            person.address = Console.ReadLine();
+            Console.WriteLine("Enter city");
+            person.city = Console.ReadLine();
+            Console.WriteLine("Enter state");
+            person.state = Console.ReadLine();
+            Console.WriteLine("Enter zipCode");
+            person.zipCode = Console.ReadLine();
+            Console.WriteLine("Enter phone number");
+            person.phoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter emaiId");
+            person.emailId = Console.ReadLine();
+
+            listgetDetail.Add(person);
 
         }
-
-        public void GetPersonDetail()
+        public void DispalyAddressBook()
         {
-            Dictionary<String, string> myaddbook = new Dictionary<String, string>();
-            myaddbook.Add("FirstName", addbook.FirstName);
-            myaddbook.Add("LastName", addbook.LastName);
-            myaddbook.Add("Address", addbook.Address);
-            myaddbook.Add("City", addbook.City);
-            myaddbook.Add("State", addbook.State);
-            myaddbook.Add("zipcode", addbook.zipcode);
-            myaddbook.Add("PhoneNumber", addbook.PhoneNumber);
-            myaddbook.Add("EmailID", addbook.EmailId);
+            foreach (Person person in listgetDetail)
+            {
+                Console.WriteLine("first Name: " + person.firstName);
+                Console.WriteLine("last Name: " + person.lastName);
+                Console.WriteLine("Address: " + person.address);
+                Console.WriteLine("City: " + person.city);
+                Console.WriteLine("State: " + person.state);
+                Console.WriteLine("Zip code: " + person.zipCode);
+                Console.WriteLine("PhoneNumber: " + person.phoneNumber);
+                Console.WriteLine("EmailId: " + person.emailId);
+            }
+        }
+
+        public void EditAddressBook(string firstname)
+        {
             
-            foreach (KeyValuePair<String, string> item in myaddbook)
+            foreach (Person person in listgetDetail)
             {
-                Console.WriteLine(item.Key + " " + item.Value);
-            }
-        }        
+                if (firstname == person.firstName)
+                {
+                    Console.WriteLine("Enter first name");
+                    person.firstName = Console.ReadLine();
+                    Console.WriteLine("Enter Last name");
+                    person.lastName = Console.ReadLine();
+                    Console.WriteLine("Enter address");
+                    person.address = Console.ReadLine();
+                    Console.WriteLine("Enter city");
+                    person.city = Console.ReadLine();
+                    Console.WriteLine("Enter state");
+                    person.state = Console.ReadLine();
+                    Console.WriteLine("Enter zipCode");
+                    person.zipCode = Console.ReadLine();
+                    Console.WriteLine("Enter phone number");
+                    person.phoneNumber = Console.ReadLine();
+                    Console.WriteLine("Enter emaiId");
+                    person.emailId = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Contact not found!!");
+                }                
+            }                 
+        }
     }
-
-    class Program
+   class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            PersonDetail perdeatil = new PersonDetail();
-            perdeatil.SetPersonDetail();
-            perdeatil.GetPersonDetail();
-            Console.WriteLine("enter 'y' for Add New Contact");
-            string ch = Console.ReadLine();
-            if (ch == "y")
+            Console.WriteLine("Welcome to Address book management system!");
+            PersonDetail persongetDetail = new PersonDetail();
+            persongetDetail.PersonAddress();
+            int choice;
+            do
             {
-                perdeatil.SetPersonDetail();
-            }
+                Console.WriteLine("Enter your choice");
+                
+                Console.WriteLine("1) Add a new contact to address book");
+                Console.WriteLine("2) Display address book");
+                Console.WriteLine("3) Edit existing contact using person's first name ");
+                Console.WriteLine("4) Exit");
+               
+                choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                   
+                    case 1:
+                        persongetDetail.PersonAddress();                        
+                        break;
+                    case 2:
+                        Console.WriteLine("Address book is ");
+                        persongetDetail.DispalyAddressBook();
+                        break;
+                    case 3:
+                        Console.WriteLine("Enter first name");
+                        string firstname = Console.ReadLine();
+                        persongetDetail.EditAddressBook(firstname);
+                        break;
+                    case 4:
+                        Console.WriteLine("Exit");
+                        break;                    
+                }
+            }while (choice != 4) ;
+
         }
     }
 }
+
